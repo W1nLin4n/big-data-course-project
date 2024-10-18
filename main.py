f18229264ba9f41ff3bf7214be91162015b0a9a3
W1nLin4n <grechkaartema@gmail.com>
2024-10-18 16:30:47 +0300

    Added printing of test dataframe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
+from basic_dfs.basic_df_Hrechka import basic_test_df
+
+
 def get_session() -> SparkSession:
     return (SparkSession
             .builder
@@ -11,3 +14,4 @@ def get_session() -> SparkSession:
 
 if __name__ == "__main__":
     session = get_session()
+    basic_test_df(session).show()
