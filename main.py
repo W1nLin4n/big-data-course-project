385835ec4eb05d7416b83b29e6cd7c9b3a30436b
W1nLin4n <grechkaartema@gmail.com>
2024-11-08 17:23:44 +0200

    Updated main function to read all dataframes

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
-from basic_dfs.basic_df_Hrechka import basic_test_df
+from df_io import read_name_basics_df, read_title_akas_df, read_title_basics_df, read_title_crew_df, \
+    read_title_episode_df, read_title_principals_df, read_title_ratings_df
 
+conf = SparkConf().setMaster("local[*]").setAppName("Task App")
 
 def get_session() -> SparkSession:
     return (SparkSession
             .builder
-            .master("local")
-            .appName("task app")
-            .config(conf=SparkConf())
+            .config(conf=conf)
             .getOrCreate())
 
 if __name__ == "__main__":
-    session = get_session()
-    basic_test_df(session).show()
+    spark = get_session()
+    name_basics = read_name_basics_df(spark)
+    title_akas = read_title_akas_df(spark)
+    title_basics = read_title_basics_df(spark)
+    title_crew = read_title_crew_df(spark)
+    title_episode = read_title_episode_df(spark)
+    title_principals = read_title_principals_df(spark)
+    title_ratings = read_title_ratings_df(spark)
